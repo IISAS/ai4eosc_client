1936a658e4ba3dce296aaeb9b2a713c3a510b898
ondrej <ondrej.habala@gmail.com>
2023-05-18 13:32:11 +0200

    deployments changed to command-subcommand structure

--- cli/deployments.py
+++ cli/deployments.py
@@ -1,76 +1,101 @@
+import typer
 import json
-
 from api.auth import BearerToken
 from api.deployments import get_deployments, get_deployment, create_deployment, delete_deployment
 from api.tools import show_response
+import settings
 
 
-def deployments_impl(
-        deployment_uuid: str,
-        api_url: str,
-        auth_token: str,
-        create: bool,
-        delete: bool,
-        new_deployment: str):
-    '''
-    Accesses the deployments interface of AI4EOSC PAPI
-    Possible uses:
-    <program name> deployments - will list deployments
-    <program name> deployments uudi - will provide info about a deployment
-    <program name> deployments --delete uuid - will delete a deployment
-    <program name> deployments --create --new-deployment <file> - will create a new deployment based on the provided file
+app = typer.Typer()
 
-    :param deployment_uuid: deployment UUID
-    :param api_url: AI4EOSC PAPI URL
-    :param auth_token: authorization token
-    :param create: flag saying that we're creating new deployment (in that case @new_deployment is also needed)
-    :param delete: flag that we're deleting a deployment
-    :param new_deployment: file with information about new deployment
-    :return: nothing
-    '''
+@app.command()
+def list(
+        api_url: str = typer.Option(
+            settings.AI4EOSC_PAPI_URL,
+            help="AI4EOSC PAPI URL",
+            envvar='AI4EOSC_PAPI_URL'),
+        auth_token: str = typer.Option(
+            None,
+            '--auth-token', '-t',
+            help='authorization bearer token',
+            envvar='AI4EOSC_PAPI_TOKEN')):
+        if auth_token is not None:
+            auth_object = BearerToken(auth_token)
+        else:
+            auth_object = None
+        response = get_deployments(api_url, auth_object)
+        show_response(response)
+
+@app.command()
+def show(
+        deployment_uuid: str = typer.Argument(
+            None,
+            help='deployment UUID'),
+        api_url: str = typer.Option(
+            settings.AI4EOSC_PAPI_URL,
+            help="AI4EOSC PAPI URL",
+            envvar='AI4EOSC_PAPI_URL'),
+        auth_token: str = typer.Option(
+            None,
+            '--auth-token', '-t',
+            help='authorization bearer token',
+            envvar='AI4EOSC_PAPI_TOKEN')):
     if auth_token is not None:
         auth_object = BearerToken(auth_token)
     else:
         auth_object = None
-    if (deployment_uuid is None) and (not create) and (not delete) and (new_deployment is None):
-        #list deployments
-        cli_list_deployments(api_url, auth_object)
-    elif (deployment_uuid is not None) and (not create) and (not delete) and (new_deployment is None):
-        #show deployment info
-        cli_show_deployment(deployment_uuid, api_url, auth_object)
-    elif (deployment_uuid is not None) and delete and (not create) and (new_deployment is None):
-        #delete deployment
-        cli_delete_deployment(deployment_uuid, api_url, auth_object)
-    elif (deployment_uuid is None) and create and (not delete) and (new_deployment is not None):
-        #create deployment
-        cli_create_deployment(api_url, auth_object, new_deployment)
-    else:
-        print('Error: incorrect combination of options and arguments')
-
-
-def cli_list_deployments(api_url, auth_object):
-    response = get_deployments(api_url, auth_object)
-    show_response(response)
-
-
-def cli_show_deployment(deployment_uuid, api_url, auth_object):
     response = get_deployment(api_url, deployment_uuid, auth_object)
     show_response(response)
 
-
-def cli_delete_deployment(deployment_uuid, api_url, auth_object):
-    response = delete_deployment(api_url, deployment_uuid, auth_object)
-    show_response(response)
-
-def cli_create_deployment(api_url, auth_object, deployment_def):
+@app.command()
+def create(
+        new_deployment: str = typer.Option(
+            None,
+            '--new-deployment', '-nd',
+            help='name of JSON file with new deployment specification'),
+        api_url: str = typer.Option(
+            settings.AI4EOSC_PAPI_URL,
+            help="AI4EOSC PAPI URL",
+            envvar='AI4EOSC_PAPI_URL'),
+        auth_token: str = typer.Option(
+            None,
+            '--auth-token', '-t',
+            help='authorization bearer token',
+            envvar='AI4EOSC_PAPI_TOKEN')):
+    if auth_token is not None:
+        auth_object = BearerToken(auth_token)
+    else:
+        auth_object = None
     dd_dict = None
     try:
-        with open(deployment_def, 'rt') as dd_file:
+        with open(new_deployment, 'rt') as dd_file:
             dd_json = dd_file.read()
             dd_dict = json.loads(dd_json)
+            response = create_deployment(api_url, dd_dict, auth_object)
+            show_response(response)
     except Exception as e:
         print(f'Error: {str(e)}')
         return
 
-    response = create_deployment(api_url, dd_dict, auth_object)
+@app.command()
+def delete(
+        deployment_uuid: str = typer.Argument(
+            None,
+            help='deployment UUID'),
+        api_url: str = typer.Option(
+            settings.AI4EOSC_PAPI_URL,
+            help="AI4EOSC PAPI URL",
+            envvar='AI4EOSC_PAPI_URL'),
+        auth_token: str = typer.Option(
+            None,
+            '--auth-token', '-t',
+            help='authorization bearer token',
+            envvar='AI4EOSC_PAPI_TOKEN')):
+    if auth_token is not None:
+        auth_object = BearerToken(auth_token)
+    else:
+        auth_object = None
+    response = delete_deployment(api_url, deployment_uuid, auth_object)
     show_response(response)
+
+
--- cli/main.py
+++ cli/main.py
@@ -1,14 +1,11 @@
-#todo Pouzivaj jednoduchy config subor pre default hodnot (PAPI endpoint, vo, oidc-agent konto, ..). Priority: default hodnoty v kode < default hodnoty v config subor < hodnoty parametrov nastavene v envvar < hodnoty parametrov zadane cez command-line. Neurobil som to pre fedcloudclient od zaciatku a teraz tazko upravujem.
-#todo Pouzvaj log file pre debug informacie. Dhlsie chybove spravy sa nedaju citat na konzole. V pripade staci nastavit log_file = stderr. To tiez nemam urobene pre fedcloudclient.
-import json
-
 import typer
 import settings
 from info import info_impl
 from modules import modules_impl
-from deployments import deployments_impl
+import deployments
 
 #todo solve how to use only --version, without any command
+#todo Pouzvaj log file pre debug informacie. Dhlsie chybove spravy sa nedaju citat na konzole. V pripade staci nastavit log_file = stderr. To tiez nemam urobene pre fedcloudclient.
 
 __version__ = '0.1.0'
 state = {
@@ -16,6 +13,7 @@ state = {
 }
 
 app = typer.Typer(help=f"AI4EOSC command-line client, version {__version__}", no_args_is_help=True)
+app.add_typer(deployments.app, name='deployment')
 
 @app.command()
 def info(
@@ -64,34 +62,6 @@ def modules(
         ):
     return modules_impl(module_name, api_url, auth_token, summary, update, new_metadata)
 
-@app.command()
-def deployments(
-        deployment_uuid: str = typer.Argument(
-            None,
-            help='deployment UUID'),
-        api_url: str = typer.Option(
-            settings.AI4EOSC_PAPI_URL,
-            help="AI4EOSC PAPI URL",
-            envvar='AI4EOSC_PAPI_URL'),
-        auth_token: str = typer.Option(
-            None,
-            '--auth-token', '-t',
-            help='authorization bearer token',
-            envvar='AI4EOSC_PAPI_TOKEN'),
-        create: bool = typer.Option(
-            False,
-            '--create', '-c',
-            help='create a deployment using provided JSON file'),
-        delete: bool = typer.Option(
-            False,
-            '--delete', '-d',
-            help='delete the named deployment'),
-        new_deployment: str = typer.Option(
-            None,
-            '--new-deployment', '-nd',
-            help='name of JSON file with new deployment specification')
-        ):
-    return deployments_impl(deployment_uuid, api_url, auth_token, create, delete, new_deployment)
 
 @app.callback()
 def main(
